822ad18eb3f58ef7b57c948a3d6594a816a34334
izumimatsuo <gurux2daddy@gmail.com>
2019-02-24 13:42:09 +0900

    add ntp, sshd, firewalld

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 CentOS 7 の初期設定をおこなう ansible role です。
 
+* selinux の設定
+  * disabled
+* locale の設定
+  * ja_JP.UTF-8
+* timezone and ntp の設定
+  * Asia/Tokyo
+* sshd の設定
+  * ed25519の鍵だけを許可
+* firewall の設定
+  * データを持たないパケットの接続を破棄
+  * SYN flood 攻撃と思われる接続を破棄
+  * ステルススキャンと思われる接続を破棄
+* カーネルパラメータの設定
+  * ipv6 の無効化
+  * SYN flood 攻撃対策
+  * Smurf 攻撃対策
+
 ## 設定項目
 
 以下の設定項目は上書き可能。
@@ -10,15 +27,3 @@ CentOS 7 の初期設定をおこなう ansible role です。
 -----------------|------------|----------
 osinit_timezone  |Asia/Tokyo  |タイムゾーン
 osinit_locale    |ja_JP.UTF-8 |言語・文字コード
-
-## ビルド
-
-以下のいづれかで ansible-playbook と testinfra を実行可能。
-
-1) docker-compose でビルド実行
-
-``` $ ./build.sh ```
-
-2) gitlab-runner でビルド実行
-
-``` $ gitlab-runner exec docker --docker-volumes /var/run/docker.sock:/var/run/docker.sock ansible_build ```
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,59 +6,76 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_os_info_distribution(host):
-    assert host.system_info.distribution == "centos"
-    assert "7.6" in host.check_output("cat /etc/redhat-release")
+def test_os_env_distribution(host):
+    assert host.system_info.distribution == 'centos'
+    assert '7.6' in host.check_output('cat /etc/redhat-release')
 
 
 def test_os_env_selinux(host):
-    assert "Disabled" in host.check_output("getenforce")
+    assert 'Disabled' in host.check_output('getenforce')
 
 
 def test_os_env_timezone(host):
-    assert "Asia/Tokyo" in host.check_output("timedatectl | grep 'Time zone:'")
+    assert 'Asia/Tokyo' in host.check_output('timedatectl | grep "Time zone:"')
 
 
 def test_os_env_locale(host):
-    assert "ja_JP.UTF-8" in host.check_output("localectl | grep Locale")
+    assert 'ja_JP.UTF-8' in host.check_output('localectl | grep Locale')
 
 
-def test_chrony_is_installed(host):
-    chrony = host.package("chrony")
-    assert chrony.is_installed
+def test_os_env_kernel_parameters(host):
+    assert 1 == host.sysctl('net.ipv6.conf.all.disable_ipv6')
+    assert 1 == host.sysctl('net.ipv6.conf.default.disable_ipv6')
+    assert 1 == host.sysctl('net.ipv4.tcp_syncookies')
+    assert 1 == host.sysctl('net.ipv4.icmp_echo_ignore_broadcasts')
 
 
-def test_chrony_running_and_enabled(host):
-    chrony = host.service("chronyd")
-    assert chrony.is_running
-    assert chrony.is_enabled
+def test_os_ntp_is_installed(host):
+    package = host.package('chrony')
+    assert package.is_installed
 
 
-def test_chrony_is_listen(host):
-    assert host.socket("udp://0.0.0.0:123").is_listening
+def test_os_ntp_running_and_enabled(host):
+    service = host.service('chronyd')
+    assert service.is_running
+    assert service.is_enabled
 
 
-def test_sshd_is_installed(host):
-    sshd = host.package("openssh-server")
-    assert sshd.is_installed
+def test_os_ntp_is_listen(host):
+    assert host.socket('udp://0.0.0.0:123').is_listening
 
 
-def test_sshd_running_and_enabled(host):
-    sshd = host.service("sshd")
-    assert sshd.is_running
-    assert sshd.is_enabled
+def test_os_sshd_is_installed(host):
+    package = host.package('openssh-server')
+    assert package.is_installed
 
 
-def test_sshd_is_listen(host):
-    assert host.socket("tcp://0.0.0.0:22").is_listening
+def test_os_sshd_running_and_enabled(host):
+    service = host.service('sshd')
+    assert service.is_running
+    assert service.is_enabled
 
 
-def test_firewalld_is_installed(host):
-    firewalld = host.package("firewalld")
-    assert firewalld.is_installed
+def test_os_sshd_is_listen(host):
+    assert host.socket('tcp://0.0.0.0:22').is_listening
 
 
-def test_firewalld_running_and_enabled(host):
-    firewalld = host.service("firewalld")
-    assert firewalld.is_running
-    assert firewalld.is_enabled
+def test_os_firewalld_is_installed(host):
+    package = host.package('firewalld')
+    assert package.is_installed
+
+
+def test_os_firewalld_running_and_enabled(host):
+    service = host.service('firewalld')
+    assert service.is_running
+    assert service.is_enabled
+
+
+def test_os_firewalld_rules(host):
+    cmd = 'iptables -L INPUT_direct | grep '
+    rule1 = cmd + '"tcp flags:FIN,SYN,RST,PSH,ACK,URG/NONE"'
+    rule2 = cmd + '"tcp flags:!FIN,SYN,RST,ACK/SYN state NEW"'
+    rule3 = cmd + '"tcp flags:FIN,SYN,RST,PSH,ACK,URG/FIN,SYN,RST,PSH,ACK,URG"'
+    assert 0 == host.run(rule1).rc
+    assert 0 == host.run(rule2).rc
+    assert 0 == host.run(rule3).rc
