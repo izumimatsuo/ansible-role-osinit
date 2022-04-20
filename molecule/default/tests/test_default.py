67e141a911bbbb698cac1af38bb8a26897d0911f
izumimatsuo <gurux2daddy@gmail.com>
2022-04-20 22:05:52 +0900

    remove kernel parameters setting

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -25,19 +25,6 @@ def test_os_env_locale(host):
     assert 'ja_JP.UTF-8' in host.check_output('localectl | grep Locale')
 
 
-def test_os_env_kernel_parameters(host):
-    assert 1 == host.sysctl('net.ipv6.conf.all.disable_ipv6')
-    assert 1 == host.sysctl('net.ipv6.conf.default.disable_ipv6')
-    assert 1 == host.sysctl('net.ipv4.icmp_echo_ignore_broadcasts')
-    assert 1 == host.sysctl('net.ipv4.conf.all.rp_filter')
-    assert 1 == host.sysctl('net.ipv4.conf.default.rp_filter')
-    assert 0 == host.sysctl('net.ipv4.conf.all.accept_redirects')
-    assert 0 == host.sysctl('net.ipv4.conf.default.accept_redirects')
-    assert 1 == host.sysctl('net.ipv4.conf.all.log_martians')
-#    assert 0 == host.sysctl('net.ipv4.ip_forward')
-    assert 2 == host.sysctl('kernel.randomize_va_space')
-
-
 def test_os_ntp_is_installed(host):
     package = host.package('chrony')
     assert package.is_installed
